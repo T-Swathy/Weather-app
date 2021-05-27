02bd33d83fcf263a728dff37dddf9a2a8aea71a0
T-Swathy <swatses2001@gmail.com>
2021-05-27 19:31:59 +0530

    Updates

---
+++ precache-manifest.79ee5f1cbe72a1fae847e59ce488a8ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543e9ad934afa2333581ec89e8e75520",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "c3428a40460aeb1967f7",
+    "url": "/Weather-app/static/css/2.37c27f5c.chunk.css"
+  },
+  {
+    "revision": "c3428a40460aeb1967f7",
+    "url": "/Weather-app/static/js/2.79e2663d.chunk.js"
+  },
+  {
+    "revision": "d4ead00aea2cc370d694511b91cfe641",
+    "url": "/Weather-app/static/js/2.79e2663d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7adadc5f8ac47fb13894",
+    "url": "/Weather-app/static/js/main.c111d393.chunk.js"
+  },
+  {
+    "revision": "881726af959c3f3d78d4",
+    "url": "/Weather-app/static/js/runtime-main.b3170915.js"
+  }
+]);
